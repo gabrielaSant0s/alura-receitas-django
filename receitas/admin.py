d05595cacf9a2177ccab4438af7d9068b471d51d
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-07-29 19:39:41 -0300

    mudando a apresentação do objeto no admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Receita
 
 # Register your models here.
-admin.site.register(Receita)
+
+# Isso faz com que os objetos do admin sejam melhor identificados
+
+
+class ListandoReceitas(admin.ModelAdmin):
+    # exibe esses atibrutos ao invés de object1, object2 e etc
+    list_display = ('id', 'nome_receita', 'categoria')
+    # deixa os atributos do objeto clicáveis
+    list_display_links = ('id', 'nome_receita')
+
+
+admin.site.register(Receita, ListandoReceitas)
