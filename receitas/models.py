9bef7bb08ab2885f9c60d72e53a23020d0c59589
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-07-29 22:42:38 -0300

    criei entidade pessoa, vinculei a entidade receita e trouxe para o front a pessoa

--- receitas/models.py
+++ receitas/models.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 from django.db import models
-from datetime import datetime
+from pessoas.models import Pessoa
+
 
 # Create your models here.
 
 
 class Receita(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
