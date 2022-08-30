ba325bac170e5ea6da8e14f5c9f8194d51cb7bdc
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-08-30 20:20:51 -0300

    django auth criando login funcional

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 import re
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 def cadastro(request):
@@ -35,6 +36,22 @@ def cadastro(request):
 
 
 def login(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        senha = request.POST["senha"]
+
+        if email == "" or senha == "":
+            print("Os campos email e senha não podem ficar em branco")
+            return redirect('login')
+
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list(
+                'username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                print('Login realizado com sucesso')
+                return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
 
@@ -43,4 +60,4 @@ def logout(request):
 
 
 def dashboard(request):
-    pass
+    return render(request, 'usuarios/dashboard.html')
