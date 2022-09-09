bd14ede166d00091801d9abb2f036dea2776b37b
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-09-08 21:08:27 -0300

    criacao de alertas e melhorias do codigo

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,7 @@
 import re
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
 from receitas.models import Receita
 
 
@@ -13,22 +13,26 @@ def cadastro(request):
         senha = request.POST["password"]
         senha2 = request.POST["password2"]
 
-        if not nome.strip():
-            print("O campo nome não pode ficar em branco")
+        if campo_vazio(nome):
+            messages.error(request, "O campo nome não pode ficar em branco")
             return redirect("cadastro")
-        if not email.strip():
-            print("O campo email não pode ficar em branco")
+        if campo_vazio(email):
+            messages.error(request, "O campo email não pode ficar em branco")
             return redirect("cadastro")
-        if senha2 != senha:
-            print("Senhas diferentes")
+        if senhas_nao_sao_iguais(senha, senha2):
+            messages.error(request, "Senhas diferentes")
             return redirect("cadastro")
         if User.objects.filter(email=email).exists():
-            print('Usuário já cadastrado')
+            messages.error(request, 'Usuário já cadastrado')
+            return redirect("cadastro")
+        if User.objects.filter(username=nome).exists():
+            messages.error(request, 'Usuário já cadastrado')
             return redirect("cadastro")
         user = User.objects.create_user(
             username=nome, email=email, password=senha)
 
         user.save()
+        messages.success(request, 'usuario cadastrado com sucesso')
         print('usuario cadastrado com sucesso')
 
         return redirect('login')
@@ -41,8 +45,9 @@ def login(request):
         email = request.POST["email"]
         senha = request.POST["senha"]
 
-        if email == "" or senha == "":
-            print("Os campos email e senha não podem ficar em branco")
+        if campo_vazio(email) or campo_vazio(senha):
+            messages.error(
+                request, "Os campos email e senha não podem ficar em branco")
             return redirect('login')
 
         if User.objects.filter(email=email).exists():
@@ -51,7 +56,7 @@ def login(request):
             user = auth.authenticate(request, username=nome, password=senha)
             if user is not None:
                 auth.login(request, user)
-                print('Login realizado com sucesso')
+                messages.success(request, 'Login realizado com sucesso')
                 return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
@@ -92,3 +97,11 @@ def cria_receita(request):
         return redirect('dashboard')
     else:
         return render(request, 'usuarios/cria_receita.html')
+
+
+def campo_vazio(campo):
+    return not campo.strip()
+
+
+def senhas_nao_sao_iguais(senha, senha2):
+    return senha != senha2
